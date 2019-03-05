3277740135208a19a5b7a08705f2e2aecb47925c
Adam Kearney <aklives@hotmail.com>
2019-03-05 03:32:43 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+var divs = document.querySelectorAll('div');
+
+function capture(e){
+  console.log(this.firstChild.nodeValue.trim() + 'captured');
+}
+
+for (let i = 0; i < divs.length; i++){
+  divs[i].addEventListener('click', capture, true)
+}
